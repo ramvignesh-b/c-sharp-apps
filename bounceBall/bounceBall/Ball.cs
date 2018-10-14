58b7bff729342f66645c590a7ed66801fb784262
D4rthVad3r <43937419+D4rthVad3r@users.noreply.github.com>
2018-10-15 04:15:14 +0530

    Add files via upload
    A C# app to create multiple balls (max=50) on click, that moves and rebounds on hitting the boundary.

---
+++ bounceBall/bounceBall/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bounceBall
+{
+    class Ball
+    {
+        Random random = new Random();
+        int x, y, vx, vy, size;
+        Brush b;
+
+        public Ball(int _x, int _y, int _vx, int _vy, int _size)
+        {
+            x = _x;
+            y = _y;
+            vx = _vx;
+            vy = _vy;
+            size = _size;
+        }
+
+        public void MoveBall(int _width, int _height)
+        {
+            x += vx;
+            y += vy;
+            if (x > _width - size || x < 0)
+            {
+                vx = -vx;
+            }
+            if (y > _height - size || y < 0)
+            {
+                vy = -vy;
+            }
+        }
+
+        public void Draw(Graphics g, Brush b)
+        {
+            g.FillEllipse(b, x, y, size, size);
+        }
+    }
+}
---
+++ bounceBall/bounceBall/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bounceBall
+{
+    public partial class Form1 : Form
+    {
+        Color color = Color.Black;
+        Graphics g;
+        Ball[] ball = new Ball[50];
+        int size = 50, count = 0, vx = 5, vy = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            ball[count] = new Ball(e.X - (size / 2), e.Y - (size / 2), vx, vy, size);
+            count++;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g.Clear(DefaultBackColor);
+            for(int i = 0; i < count; i++)
+            {
+                ball[i].Draw(g, new SolidBrush(color));
+                ball[i].MoveBall(ClientSize.Width, ClientSize.Height);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
